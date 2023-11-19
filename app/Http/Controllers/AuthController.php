5ab380d4099c7ea9fc42a0807709d5935b783806
Recky Anderson <reckyanderson@gmail.com>
2023-11-19 10:47:25 +0700

    update more cleaner & concise auth functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,40 +11,36 @@ class AuthController extends Controller
 {
     public function index()
     {
-        if (Auth::check() == true) {
-            return redirect()->route('dashboard');
+        if (Auth::check()) {
+            return $this->redirectToDashboard();
         }
         return view('auth.index');
     }
 
     public function proses(Request $request)
     {
+        // Validate the request
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
         $email = $request->email;
         $password = $request->password;
 
         $user = User::where('email', $email)->first();
+
         if ($user) {
             if (Hash::check($password, $user->password)) {
                 Auth::attempt(['email' => $email, 'password' => $password]);
                 if (Auth::check() == true) {
-                    return redirect()->route('dashboard');
+                    return $this->redirectToDashboard();
                 }
             } else {
-                session()->flash('pesan', '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <strong>Password salah !!!</strong>
-        </div>');
+                session()->flash('error', 'Password salah!');
                 return redirect()->route('login');
             }
         } else {
-            session()->flash('pesan', '<div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <strong>Email tidak ditemukan !!!</strong>
-        </div>');
+            session()->flash('warning', 'Email tidak ditemukan!');
             return redirect()->route('login');
         }
     }
@@ -52,12 +48,23 @@ class AuthController extends Controller
     public function logout()
     {
         Auth::logout();
-        session()->flash('pesan', '<div class="alert alert-info alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-            <strong>Logout berhasil !!!</strong>
-        </div>');
+        session()->flash('info', 'Berhasil logout!');
         return redirect()->route('login');
+    }
+
+    private function redirectToDashboard()
+    {
+        $user = Auth::user();
+
+        switch ($user->getRoleNames()->first()) {
+            case 'admin':
+                return redirect()->route('admin.dashboard');
+            case 'prodi':
+                return redirect()->route('prodi.dashboard');
+            case 'auditor':
+                return redirect()->route('auditor.dashboard');
+            default:
+                abort(403, 'Unauthorized action.');
+        }
     }
 }
