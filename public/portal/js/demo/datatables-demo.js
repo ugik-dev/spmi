95fc357935359a32c211ebc5ff9c5678d1462ea9
Recky Anderson <reckyanderson@gmail.com>
2023-11-20 00:59:32 +0700

    remove files

--- public/portal/js/demo/datatables-demo.js
+++
@@ -1,9 +0,0 @@
-// Call the dataTables jQuery plugin
-$(document).ready(function() {
-    // var tabel = $('#dataTable').DataTable();
-    // var order = table.order([1, 'asc']).draw()
-
-    $('#dataTable').DataTable({
-        "order": [0, 'asc']
-    });
-});
