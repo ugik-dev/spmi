d3ce97285b1ad9d425e2522fcc25618576779232
Recky Anderson <reckyanderson@gmail.com>
2023-11-19 15:52:54 +0700

    create degree model,controller (resource),migration

---
+++ app/Http/Controllers/DegreeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Degree;
+use Illuminate\Http\Request;
+
+class DegreeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Degree  $degree
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Degree $degree)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Degree  $degree
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Degree $degree)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Degree  $degree
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Degree $degree)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Degree  $degree
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Degree $degree)
+    {
+        //
+    }
+}
