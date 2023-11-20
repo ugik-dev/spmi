21eb75bcaa1becd4f83ddba7f14ea3d4d4beec6d
Recky Anderson <reckyanderson@gmail.com>
2023-11-20 19:41:41 +0700

    make a separate file to include in baseview layout

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>&copy; Copyright <strong><span>LPM SMART SYSTEM</span></strong>. All Rights
+                Reserved</span>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/template/logout-modal.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <form action="{{ route('logout') }}" method="POST">
+        @csrf
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Sipa untuk logout?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Jika anda yakin ingin keluar silahkan klik logout</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <button class="btn btn-primary" type="submit">Logout</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
