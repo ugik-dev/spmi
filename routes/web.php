68afd1894243eaab8e969f164deb97150283de22
Recky Anderson <reckyanderson@gmail.com>
2023-11-21 19:57:28 +0700

    add routes

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,19 @@ Route::post('proses', 'AuthController@proses');
 Route::post('logout', 'AuthController@logout')->name('logout');
 
 Route::get('/', function () {
-    return redirect()->route('login');
+  return redirect()->route('login');
 });
 // Authenticated User With Checking Roles Middleware
 Route::middleware(['auth'])->group(function () {
 
-    // DASHBOARD
-    Route::get('/dasbor', 'Auth\DashboardController@index')->name('dashboard');
-    Route::middleware(['can:see users'])->group(function () {
-        Route::get('/pengguna', 'UserController@index')->name('users.index');
-    });
+  // DASHBOARD
+  Route::get('/dasbor', 'Auth\DashboardController@index')->name('dashboard');
+
+  Route::middleware(['can:see users'])->group(function () {
+    Route::get('/pengguna', 'UserController@index')->name('users.index');
+  });
+
+  Route::patch('/edit-pengguna/{user}', 'UserController@edit')->middleware('can:edit user')->name('users.edit');
+  Route::delete('/hapus-pengguna/{user}', 'UserController@delete')->middleware('can:delete user')->name('users.delete');
+  Route::get('detail-pengguna/{user}', 'UserController@detail')->middleware('can:see user')->name('users.detail');
 });
