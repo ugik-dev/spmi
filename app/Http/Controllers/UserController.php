4de7e9f944cd1472280ec5dbbb85ebf6a9fd9802
Recky Anderson <reckyanderson@gmail.com>
2023-11-21 21:54:19 +0700

    modify role validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,15 +25,16 @@ class UserController extends Controller
     $validatedData = $request->validate([
       'name' => 'required|max:255',
       'email' => 'required|email|max:255|unique:users,email,' . $user->id,
-      'role' => 'in:admin,auditor,prodi',
+      'role' => 'nullable|in:admin,auditor,prodi',
       'password' => 'nullable|min:6',
     ]);
 
     if (!empty($validatedData['password'])) {
       $user->password = bcrypt($validatedData['password']);
     }
-
-    $user->syncRoles($validatedData['role']);
+    if (!empty($validatedData['role'])) {
+      $user->syncRoles($validatedData['role']);
+    }
     $user->email = $validatedData['email'];
     $user->name = $validatedData['name'];
     $user->save();
