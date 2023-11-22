eb83e0d887898186e6009ac8f4b1b5ecad299679
Recky Anderson <reckyanderson@gmail.com>
2023-11-22 23:36:32 +0700

    laravel ui, degree, faculty routes

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,49 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Auth\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DegreeController;
+use App\Http\Controllers\FacultyController;
 
-// Auth
-Route::get('login', 'AuthController@index')->name('login');
-Route::post('proses', 'AuthController@proses');
-Route::post('logout', 'AuthController@logout')->name('logout');
+// Auth Routes
+// Route::get('login', [AuthController::class, 'index'])->name('login');
+// Route::post('proses', [AuthController::class, 'proses']);
+// Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/', function () {
-  return redirect()->route('login');
-});
-// Authenticated User With Checking Roles Middleware
-Route::middleware(['auth'])->group(function () {
+Route::redirect('/', '/login');
 
-  // DASHBOARD
-  Route::get('/dasbor', 'Auth\DashboardController@index')->name('dashboard');
+// Authenticated User Routes
+Route::middleware('auth')->group(function () {
 
-  Route::middleware(['can:see users'])->group(function () {
-    Route::get('/pengguna', 'UserController@index')->name('users.index');
+  // Dashboard
+  Route::get('/dasbor', [DashboardController::class, 'index'])->name('dashboard');
+
+  // User Routes
+  Route::prefix('pengguna')->name('users.')->group(function () {
+    Route::get('/', [UserController::class, 'index'])->middleware('can:see users')->name('index');
+    Route::get('/datatables', [UserController::class, 'datatables'])->middleware('can:see users')->name('datatables');
+    Route::patch('/edit/{user}', [UserController::class, 'edit'])->middleware('can:edit user')->name('edit');
+    Route::delete('/hapus/{user}', [UserController::class, 'delete'])->middleware('can:delete user')->name('delete');
+    Route::post('/tambah', [UserController::class, 'create'])->middleware('can:create user')->name('create');
   });
 
-  Route::patch('/edit-pengguna/{user}', 'UserController@edit')->middleware('can:edit user')->name('users.edit');
-  Route::delete('/hapus-pengguna/{user}', 'UserController@delete')->middleware('can:delete user')->name('users.delete');
-  Route::get('detail-pengguna/{user}', 'UserController@detail')->middleware('can:see user')->name('users.detail');
-  Route::post('/tambah-pengguna', 'UserController@create')->middleware('can:create user')->name('users.create');
+  // Degree Routes
+  Route::prefix('jenjang')->name('degrees.')->group(function () {
+    Route::get('/', [DegreeController::class, 'index'])->middleware('can:see degrees')->name('index');
+    Route::patch('/edit/{degree}', [DegreeController::class, 'edit'])->middleware('can:edit degree')->name('edit');
+    Route::delete('/hapus/{degree}', [DegreeController::class, 'delete'])->middleware('can:delete degree')->name('delete');
+    Route::post('/tambah', [DegreeController::class, 'create'])->middleware('can:create degree')->name('create');
+  });
+
+  // Faculty Routes
+  Route::prefix('fakultas')->name('faculties.')->group(function () {
+    Route::get('/', [FacultyController::class, 'index'])->middleware('can:see faculties')->name('index');
+    Route::post('/tambah', [FacultyController::class, 'create'])->middleware('can:create faculty')->name('create');
+    Route::patch('/edit/{faculty}', [FacultyController::class, 'edit'])->middleware('can:edit faculty')->name('edit');
+    Route::delete('/hapus/{faculty}', [FacultyController::class, 'delete'])->middleware('can:delete faculty')->name('delete');
+  });
 });
+
+Auth::routes();
