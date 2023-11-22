0b374f7ab53a527e25419c58197d5222473955b8
Recky Anderson <reckyanderson@gmail.com>
2023-11-22 23:37:43 +0700

    adjusting laravel ui installation and cleaning

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,8 +3,6 @@ const mix = require("laravel-mix");
 mix
   .js("resources/js/app.js", "public/js")
   .js("resources/js/users-index.js", "public/js")
-  .js("resources/js/degrees-index.js", "public/js")
-  .js("resources/js/faculties-index.js", "public/js")
   .sass("resources/sass/app.scss", "public/css")
   .scripts(
     [
@@ -14,23 +12,13 @@ mix
       "node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js",
       "node_modules/select2/dist/js/select2.min.js",
       "node_modules/sweetalert2/dist/sweetalert2.min.js",
-      "node_modules/@popperjs/core/dist/umd/popper.min.js",
       "resources/js/vendor/jquery.easing.min.js",
       "resources/js/vendor/jquery.easing.compatibility.js",
       "resources/js/vendor/sb-admin-2.min.js",
     ],
     "public/js/vendor.js"
   )
-  .styles(
-    [
-      "node_modules/bootstrap/dist/css/bootstrap.min.css",
-      "node_modules/datatables.net-bs4/css/dataTables.bootstrap4.min.css",
-      "node_modules/select2/dist/css/select2.min.css",
-      "node_modules/sweetalert2/dist/sweetalert2.min.css",
-      "resources/css/sb-admin-2.css",
-    ],
-    "public/css/vendor.css"
-  )
+
   .copyDirectory(
     "node_modules/@fortawesome/fontawesome-free",
     "public/fontawesome"
