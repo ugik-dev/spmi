b8ce67077be1d67f33dbf9dc4b909c60f437645b
Recky Anderson <reckyanderson@gmail.com>
2023-11-22 23:33:20 +0700

    cleaning

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,17 +1,5 @@
-window._ = require('lodash');
+window._ = require("lodash");
 
-window.axios = require('axios');
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.Swal = require('sweetalert2');
-
-// import Echo from 'laravel-echo';
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.axios = require("axios");
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+window.Swal = require("sweetalert2");
