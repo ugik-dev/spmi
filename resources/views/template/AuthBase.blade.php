ea6edeb1a3e3d742ccdb0d5709366d39835936e5
Recky Anderson <reckyanderson@gmail.com>
2023-11-22 23:36:00 +0700

    adjusted with laravel ui

--- resources/views/template/AuthBase.blade.php
+++ resources/views/template/AuthBase.blade.php
@@ -1,27 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Login | SPMI Smart System</title>
-
+    <title>{{ config('app.name', 'SPMI Smart System') }}</title>
     <!-- Favicons -->
     @include('partials.favicons') <!-- Partial for favicon links -->
 
     <!-- FontAwesome -->
     <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet" type="text/css">
 
-    <!-- Vendor CSS (Bootstrap, DataTables, etc.) -->
-    <link href="{{ mix('css/vendor.css') }}" rel="stylesheet">
-
     <!-- App CSS -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
     @stack('styles')
 </head>
 
@@ -30,9 +27,6 @@
     <div class="container">
         @yield('auth')
     </div>
-
-    <!-- Vendor JS (jQuery, Bootstrap, etc.) -->
-    <script src="{{ mix('js/vendor.js') }}"></script>
 
     <!-- App JS -->
     <script src="{{ mix('js/app.js') }}"></script>
