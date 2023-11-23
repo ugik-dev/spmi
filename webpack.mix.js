a9de127b28cc33e916f95568754bd6ea6a451b7f
Recky Anderson <reckyanderson@gmail.com>
2023-11-23 18:26:27 +0700

    import vendor in bootstrap.js and add global jquery

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,23 +1,19 @@
 const mix = require("laravel-mix");
+const webpack = require("webpack");
+
+mix.webpackConfig({
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+    }),
+  ],
+});
 
 mix
   .js("resources/js/app.js", "public/js")
-  .js("resources/js/users-index.js", "public/js")
   .sass("resources/sass/app.scss", "public/css")
-  .scripts(
-    [
-      "node_modules/jquery/dist/jquery.min.js",
-      "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
-      "node_modules/datatables.net/js/jquery.dataTables.min.js",
-      "node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js",
-      "node_modules/select2/dist/js/select2.min.js",
-      "node_modules/sweetalert2/dist/sweetalert2.min.js",
-      "resources/js/vendor/jquery.easing.min.js",
-      "resources/js/vendor/jquery.easing.compatibility.js",
-      "resources/js/vendor/sb-admin-2.min.js",
-    ],
-    "public/js/vendor.js"
-  )
 
   .copyDirectory(
     "node_modules/@fortawesome/fontawesome-free",
