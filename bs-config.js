659e9aac8e79ecedac99f9986e044c4de699ba97
Recky Anderson <reckyanderson@gmail.com>
2023-11-24 20:03:49 +0700

    adjust port

--- bs-config.js
+++ bs-config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    files: [
-        'resources/views/**/*.blade.php',
-    ],
-    proxy: 'localhost:8000',
-    port: 3000, // Browsersync port (you can change this to another port if needed)
+  files: ["resources/views/**/*.blade.php"],
+  proxy: "localhost",
+  port: 3000, // Browsersync port (you can change this to another port if needed)
 };
