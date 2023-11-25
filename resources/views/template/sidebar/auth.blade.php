9d2005326c155013082d72bf2d458154523f74f3
Recky Anderson <reckyanderson@gmail.com>
2023-11-25 09:40:38 +0700

    make more beauty

--- resources/views/template/sidebar/auth.blade.php
+++ resources/views/template/sidebar/auth.blade.php
@@ -2,10 +2,11 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
-        <div class="sidebar-brand-logo">
-            <img class="img-logo" src="{{ mix('images/logo-sidebar.png') }}">
+        <div class="sidebar-brand-icon">
+            <img class="img-logo" src="{{ mix('images/logo-sidebar.png') }}" alt="Logo">
         </div>
     </a>
+
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
@@ -14,7 +15,8 @@
     <li class="nav-item {{ Route::is('dashboard') ? 'active' : '' }}">
         <a class="nav-link" href="{{ route('dashboard') }}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dasbor</span></a>
+            Dasbor
+        </a>
     </li>
 
     <!-- Divider -->
@@ -24,34 +26,36 @@
     <div class="sidebar-heading mb-2">
         Menu Manajemen
     </div>
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
 
-
+    <!-- Nav Items -->
     @can('see prodis')
         <li class="nav-item {{ Route::is('programs.index') ? 'active' : '' }}">
-            <a class="nav-link {{ Route::is('programs.index') ? 'active' : '' }}" href="{{ route('programs.index') }}"><i
-                    class="mr-2 fa-solid fa-book"></i>Program Studi</a>
+            <a class="nav-link" href="{{ route('programs.index') }}">
+                <i class="fas fa-book mr-2"></i>Program Studi
+            </a>
         </li>
     @endcan
+
     @can('see degrees')
         <li class="nav-item {{ Route::is('degrees.index') ? 'active' : '' }}">
-            <a class="nav-link {{ Route::is('degrees.index') ? 'active' : '' }}" href="{{ route('degrees.index') }}">
-                <i class="mr-2 fa-solid fa-graduation-cap"></i>Jenjang
-                Pendidikan</a>
+            <a class="nav-link" href="{{ route('degrees.index') }}">
+                <i class="fas fa-graduation-cap mr-2"></i>Jenjang Pendidikan
+            </a>
         </li>
     @endcan
+
     @can('see faculties')
         <li class="nav-item {{ Route::is('faculties.index') ? 'active' : '' }}">
-            <a class="nav-link {{ Route::is('faculties.index') ? 'active' : '' }}"
-                href="{{ route('faculties.index') }}"><i class="mr-2 fa-solid fa-building"></i>Fakultas</a>
+            <a class="nav-link" href="{{ route('faculties.index') }}">
+                <i class="fas fa-building mr-2"></i>Fakultas
+            </a>
         </li>
     @endcan
+
     @can('see users')
         <li class="nav-item {{ Route::is('users.index') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('users.index') }}">
-                <i class="fa-solid fa-user"></i>
-                <span>Pengguna</span>
+                <i class="fas fa-user mr-2"></i>Pengguna
             </a>
         </li>
     @endcan
@@ -73,8 +77,11 @@
     </li> --}}
 
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline mt-4">
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler -->
+    <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
 
