ce0e87fa13fba1a3d0e4d00aa9014fe1775cf695
Recky Anderson <reckyanderson@gmail.com>
2023-11-25 08:03:04 +0700

    add get attribute and fillable

--- app/StudyProgram.php
+++ app/StudyProgram.php
@@ -6,18 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudyProgram extends Model
 {
-    protected $guarded = ['name', 'code'];
+  protected $fillable = ['name', 'code', 'degree_id', 'faculty_id', 'vision', 'mission', 'description'];
 
-    public function users()
-    {
-        return $this->hasMany(User::class);
-    }
-    public function degree()
-    {
-        return $this->belongsTo(Degree::class);
-    }
-    public function faculty()
-    {
-        return $this->belongsTo(Faculty::class);
-    }
+  protected $casts = [
+    'vision' => 'array',
+  ];
+
+  public function users()
+  {
+    return $this->hasMany(User::class);
+  }
+  public function degree()
+  {
+    return $this->belongsTo(Degree::class);
+  }
+  public function faculty()
+  {
+    return $this->belongsTo(Faculty::class);
+  }
+  public function getDegreeNameAttribute()
+  {
+    return $this->degree ? $this->degree->name : null;
+  }
+  public function getFacultyNameAttribute()
+  {
+    return $this->faculty ? $this->faculty->name : null;
+  }
 }
