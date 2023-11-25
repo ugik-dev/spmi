a682a5750ea38fc1aabc7f8dcc79e8e5951060ee
Recky Anderson <reckyanderson@gmail.com>
2023-11-25 08:02:09 +0700

    adjust select name

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
     $validatedData = $request->validate([
       'name' => 'required|max:255',
       'email' => 'required|email|max:255|unique:users',
-      'role' => 'nullable|in:admin,auditor,prodi',
+      'roles' => 'nullable|in:admin,auditor,prodi',
       'password' => 'required|min:6',
     ]);
 
@@ -32,8 +32,8 @@ class UserController extends Controller
 
     $user->save();
 
-    if (!empty($validatedData['role'])) {
-      $user->assignRole($validatedData['role']);
+    if (!empty($validatedData['roles'])) {
+      $user->assignRole($validatedData['roles']);
     }
 
     return redirect()->route('users.index')->with('success', 'Pengguna berhasil dibuat!');
@@ -49,15 +49,15 @@ class UserController extends Controller
     $validatedData = $request->validate([
       'name' => 'required|max:255',
       'email' => 'required|email|max:255|unique:users,email,' . $user->id,
-      'role' => 'nullable|in:admin,auditor,prodi',
+      'roles' => 'nullable|in:admin,auditor,prodi',
       'password' => 'nullable|min:6',
     ]);
 
     if (!empty($validatedData['password'])) {
       $user->password = bcrypt($validatedData['password']);
     }
-    if (!empty($validatedData['role'])) {
-      $user->syncRoles($validatedData['role']);
+    if (!empty($validatedData['roles'])) {
+      $user->syncRoles($validatedData['roles']);
     }
     $user->email = $validatedData['email'];
     $user->name = $validatedData['name'];
