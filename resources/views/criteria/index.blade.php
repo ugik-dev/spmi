20e54af8796a23bb5595a879c69e2608ccb2b688
Recky Anderson <reckyanderson@gmail.com>
2023-11-27 13:52:39 +0700

    modify ajax & jquery when performs asynchronous functionality

--- resources/views/criteria/index.blade.php
+++ resources/views/criteria/index.blade.php
@@ -23,72 +23,113 @@
     @endcan
 
     {{-- Edit Study Program Modal --}}
-    {{-- @can('edit prodi')
+    @can('edit prodi')
         @include('criteria.partials.edit-modal')
-    @endcan --}}
+    @endcan
 @endsection
-
 @push('scripts')
     {{ $dataTable->scripts() }}
     <script>
-        $(function() {
-            const $criteriaTable = $("#criteria-table").DataTable();
+        async function handleSelectCriterionLevelChange(event) {
+            const level = parseInt($(this).val());
+            const parentLevel = parseInt(level - 1);
+            const criterionForm = $(event.delegateTarget);
+            const parentCriterionSelect = criterionForm.find('[name="parent"]');
+            const parentCriterionGroup = parentCriterionSelect.closest('.form-group');
+            const parentCriterionCode = criterionForm.find('[name="parent_code"]');
+            const parentCriterionCodeGroup = parentCriterionCode.closest('.form-group');
 
-            // DataTables init event
-            $criteriaTable.on('init.dt', function() {
-                // Modify button after table initialization
-                $criteriaTable.buttons('.button-add').nodes().each(function() {
-                    $(this).attr('data-toggle', 'modal');
-                    $(this).attr('data-target', '#createCriterionModal');
-                    $(this).find('.button-add-icon-placeholder').html('<i class="fa fa-plus"></i>');
-                    $(this).removeClass('btn-secondary').addClass('btn-primary');
+            if (!(level > 1)) {
+                parentCriterionGroup.addClass('d-none');
+                parentCriterionCodeGroup.addClass('d-none');
+                return Promise.resolve(); // Immediately resolve the promise
+            }
+            parentCriterionGroup.removeClass('d-none');
+            parentCriterionCodeGroup.removeClass('d-none');
+
+            try {
+                const response = await axios.get('/get-parent-criteria', {
+                    params: {
+                        level: parentLevel
+                    }
                 });
-            });
+                const options = response.data.map(item =>
+                    `<option value="${item.id}">[${item.code}] - ${item.name}</option>`);
+                parentCriterionSelect.html(options.join(''));
+                parentCriterionSelect.trigger('change');
+                return Promise.resolve(); // Resolve the promise after populating options
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                return Promise.reject(error); // Reject the promise in case of an error
+            }
+        }
 
-            $(document).ready(function() {
-                setupEditModal('#editModal', '#edit-prodi-form', '/prodi/edit/:id', 'model');
 
-                setupDeleteFunctionality(
-                    '#study_programs-table',
-                    '/prodi/hapus/:id',
-                    'model-name'
-                );
-            });
+        function handleSelectParentChange(event) {
+            const criterionForm = $(event.delegateTarget);
+            const parentCodeInput = criterionForm.find('[name="parent_code"]');
 
-        });
-    </script>
-    <script>
+            const selectedOptionText = $(this).find('option:selected').text();
+            const codeMatch = selectedOptionText.match(/\[(.*?)\]/);
+
+            if (codeMatch && codeMatch[1]) {
+                parentCodeInput.val(codeMatch[1]);
+            } else {
+                parentCodeInput.val('');
+            }
+        }
+
+        function populateEditForm(criterionData) {
+            var form = $('#edit-criterion-form');
+
+            // Populate other fields directly
+            form.find('[name="name"]').val(criterionData.name);
+            form.find('[name="code"]').val(criterionData.code);
+
+
+            setTimeout(() => {
+                form.find('[name="level"]').val(criterionData.level).change();
+
+                setTimeout(() => {
+                    form.find('[name="parent"]').val(criterionData.parentId).change();
+                }, 100); // adjust delay as necessary
+            }, 100);
+        }
         $(document).ready(function() {
-            $('#criterion-level').on('change', function() {
-                var level = $(this).val();
-                if (level > 1) {
-                    $('#parent-criterion-group').removeClass('d-none');
+            // Temporary hide sidebar to debug console
+            $('#accordionSidebar').addClass('d-none')
 
-                    axios.get('/get-parent-criteria', {
-                            params: {
-                                level: level - 1
-                            }
-                        })
-                        .then(function(response) {
-                            var parentInput = $("#criterion-parent");
-                            parentInput.empty(); // Clear previous options
+            // Cached jQuery objects for better performance
+            const $criteriaTable = window.LaravelDataTables['criteria-table'];
 
-                            // Assuming response.data is an array of criteria
-                            response.data.forEach(function(criterion) {
-                                parentInput.append(new Option(criterion.name + ' (' + criterion
-                                    .code + ')', criterion.id));
-                            });
-                        })
-                        .catch(function(error) {
-                            console.log(error);
-                            // Handle the error here, such as displaying a notification
-                        });
-                } else {
-                    $('#parent-criterion-group').addClass('d-none');
-                }
+            $criteriaTable.on('init.dt', function() {
+                modifyDataTableButtons($criteriaTable);
+                attachEditButtonEvent();
             });
 
-            // Additional logic...
+            $("#create-criterion-form").on('change', '[name="level"]', handleSelectCriterionLevelChange);
+            $("#create-criterion-form").on('change', '[name="parent"]', handleSelectParentChange);
+            $("#edit-criterion-form").on('change', '[name="level"]', handleSelectCriterionLevelChange);
+            $("#edit-criterion-form").on('change', '[name="parent"]', handleSelectParentChange);
         });
+
+        function modifyDataTableButtons($criteriaTable) {
+            $criteriaTable.buttons('.button-add').nodes().each(function() {
+                $(this).attr('data-toggle', 'modal')
+                    .attr('data-target', '#createCriterionModal')
+                    .html('<i class="fa fa-plus"></i> Tambah Kriteria')
+                    .removeClass('btn-secondary').addClass('btn-primary');
+            });
+            $(".btn.btn-secondary").addClass('my-1 my-md-0');
+            $($criteriaTable.buttons().container()[0]).addClass("text-center d-block");
+            $("#create-criterion-form select").val('').change();
+        }
+
+        function attachEditButtonEvent() {
+            $('.btn-edit').on('click', function() {
+                var criterionData = $(this).data('model');
+                populateEditForm(criterionData);
+            });
+        }
     </script>
 @endpush
