0a2e0787df69a5d4e6bed0024e6607ffe44d9029
Ugik DEV <ugik.dev@gmail.com>
2024-04-26 01:55:44 +0700

    user on dipas

--- database/migrations/2023_12_02_230155_create_dipa.php
+++ database/migrations/2023_12_02_230155_create_dipa.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use App\Models\WorkUnit;
+use App\Models\User;
 
 return new class extends Migration
 {
