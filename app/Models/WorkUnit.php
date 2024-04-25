13f89c324343e57d4647e88ab64980149039e644
Ugik DEV <ugik.dev@gmail.com>
2024-04-25 21:49:12 +0700

    re

--- app/Models/WorkUnit.php
+++ app/Models/WorkUnit.php
@@ -15,9 +15,4 @@ class WorkUnit extends Model
     {
         return $this->hasMany(UnitBudget::class);
     }
-
-    public function unitBudgets()
-    {
-        return $this->hasMany(UnitBudget::class);
-    }
 }
