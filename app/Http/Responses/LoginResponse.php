d4b498f14d040a17452931c50c871827379fad37
Ugik DEV <ugik.dev@gmail.com>
2024-04-29 23:15:08 +0700

    login to dashboard

--- app/Http/Responses/LoginResponse.php
+++ app/Http/Responses/LoginResponse.php
@@ -13,7 +13,7 @@ class LoginResponse implements LoginResponseContract
         return $request->wantsJson()
             ? response()->json(['two_factor' => false])
             : redirect()->intended(
-                auth()->user()->hasRole('SUPER ADMIN PERENCANAAN') ? route('user.index') : route('sbm_sbi.index')
+                auth()->user()->hasRole('SUPER ADMIN PERENCANAAN') ? route('user.index') : route('admin.dashboard')
             );
     }
 }
