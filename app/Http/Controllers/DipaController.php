de59554a8c937f4749f36dc735de1ea794d33b8b
Ugik DEV <ugik.dev@gmail.com>
2024-05-06 22:40:42 +0700

    res

--- app/Http/Controllers/DipaController.php
+++ app/Http/Controllers/DipaController.php
@@ -193,4 +193,39 @@ class DipaController extends Controller
             return response()->json(['error' => true, 'message' => $e->getMessage()], 500);
         }
     }
+
+    public function approval_perencanaan(Request $request, Dipa $dipa)
+    {
+        try {
+            if (
+                in_array($dipa->status, ['wait-perencanaan', 'reject-perencanaan']) &&
+                // $dipa->work_unit_id == Auth::user()->employee?->work_unit_id &&
+                Auth::user()->hasRole(['SPI'])
+            ) {
+            } else {
+                return response()->json(['error' => true,  'message' => 'Anda tidak berhak melalukan aksi ini'], 500);
+            }
+            $log = new DipaLog();
+            if ($request->res == 'Y') {
+                $dipa->status = 'accept';
+                $log->label = "success";
+                $log->description = "Melakukan Approv";
+            } else {
+                $dipa->status = 'reject-perencanaan';
+                $log->label = "danger";
+                if (!empty($request->description)) $log->description = "Melakukan Penolakan dengan alasan " . $request->description;
+                else $log->description = "Melakukan Penolakan";
+            }
+            $dipa->save();
+
+            $log->dipa_id = $dipa->id;
+            $log->user_id = Auth::user()->id;
+            $log->save();
+
+            return response()->json(['error' => false,  'message' => $request->res], 200);
+        } catch (\Exception $e) {
+            Log::error($e);
+            return response()->json(['error' => true, 'message' => $e->getMessage()], 500);
+        }
+    }
 }
